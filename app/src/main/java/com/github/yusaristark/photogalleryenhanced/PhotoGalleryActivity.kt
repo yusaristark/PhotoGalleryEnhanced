a951d701296db5ba9bf8d22d955d2f71b4a2b8ff
yusaristark <yusaristark@gmail.com>
2021-09-16 14:07:38 +0400

    notifications functionallity added

--- app/src/main/java/com/github/yusaristark/photogalleryenhanced/PhotoGalleryActivity.kt
+++ app/src/main/java/com/github/yusaristark/photogalleryenhanced/PhotoGalleryActivity.kt
@@ -1,5 +1,7 @@
 package com.github.yusaristark.photogalleryenhanced
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -17,6 +19,12 @@ class PhotoGalleryActivity : AppCompatActivity() {
         val isFragmentContainerEmpty = savedInstanceState == null
         if (isFragmentContainerEmpty) {
             supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance()).commit()
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context) : Intent {
+             return Intent(context, PhotoGalleryActivity::class.java)
         }
     }
 }
--- app/src/main/java/com/github/yusaristark/photogalleryenhanced/PollWorker.kt
+++ app/src/main/java/com/github/yusaristark/photogalleryenhanced/PollWorker.kt
@@ -1,13 +1,16 @@
 package com.github.yusaristark.photogalleryenhanced
 
+import android.app.PendingIntent
 import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 private const val TAG = "PollWorker"
 
-class PollWorker(val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+class PollWorker(private val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
         val query = QueryPreferences.getStoredQuery(context)
         val lastResultId = QueryPreferences.getLastResultId(context)
@@ -35,6 +38,19 @@ class PollWorker(val context: Context, workerParams: WorkerParameters) : Worker(
         } else {
             Log.i(TAG, "Got a new result: $resultId")
             QueryPreferences.setLastResultId(context, resultId)
+            val intent = PhotoGalleryActivity.newIntent(context)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+            val resources = context.resources
+            val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setTicker(resources.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(resources.getString(R.string.new_pictures_title))
+                .setContentText(resources.getString(R.string.new_pictures_text))
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build()
+            val notificationManager = NotificationManagerCompat.from(context)
+            notificationManager.notify(0, notification)
         }
         return Result.success()
     }
